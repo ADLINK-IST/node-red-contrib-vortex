a2f15adc5e654f3efb587009fd63533ceae02cd1
Toby McClean <toby.mcclean@gmail.com>
2015-12-11 14:52:45 -0500

    Update to reflect feedback on initial contribution

--- vortex/prismtech-vortex.js
+++ vortex/prismtech-vortex.js
@@ -1,23 +1,23 @@
 /**
-  PrismTech licenses this file to You under the Apache License, Version 2.0
-  (the "License"); you may not use this file except in compliance with the
-  License and with the PrismTech Vortex product. You may obtain a copy of the
-  License at
-  http://www.apache.org/licenses/LICENSE-2.0
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-  License and README for the specific language governing permissions and
-  limitations under the License.
-**/
-module.exports = function(RED) {
+ PrismTech licenses this file to You under the Apache License, Version 2.0
+ (the "License"); you may not use this file except in compliance with the
+ License and with the PrismTech Vortex product. You may obtain a copy of the
+ License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ License and README for the specific language governing permissions and
+ limitations under the License.
+ **/
+module.exports = function (RED) {
     //"use strict";
     var vortex = require("node-vortex");
     var events = require("events");
     var util = require("util");
 
     function VortexServerNode(n) {
-        RED.nodes.createNode(this,n);
+        RED.nodes.createNode(this, n);
         this.host = n.host;
         this.port = n.port;
 
@@ -30,226 +30,273 @@ module.exports = function(RED) {
 
         events.EventEmitter.call(this);
 
-        if(this.credentials) {
+        if (this.credentials) {
             this.username = this.credentials.user;
             this.password = this.credentials.password;
         }
 
         // Create the URL to connect to Vortex
-        if(this.vortexurl == "") {
+        if (this.vortexurl == "") {
             this.vortexurl = "ws://";
 
-            if(this.host != "") {
-                this.vortexurl = this.vortexurl+this.host+":"+this.port;
+            if (this.host != "") {
+                this.vortexurl = this.vortexurl + this.host + ":" + this.port;
             } else {
-                this.vortexurl = this.vortexurl+"localhost:9000";
+                this.vortexurl = this.vortexurl + "localhost:9000";
             }
         }
 
-        console.warn("[DEBUG] VortexServerNode -- Using url : "+this.vortexurl);
+        console.warn("[DEBUG] VortexServerNode -- Using url : " + this.vortexurl);
         // Define the functions called by Vortex in and out nodes
         var node = this;
         this.vortex = vortex;
-        this.register = function() {
+        this.register = function () {
             console.warn("[DEBUG] VortexServerNode -- register");
             node.usecount += 1;
         };
 
-        this.deregister = function() {
+        this.deregister = function () {
             console.warn("[DEBUG] VortexServerNode -- deregister");
             node.usecount -= 1;
-            if(node.usecount == 0) {
+            if (node.usecount == 0) {
                 node.client.end();
             }
         };
 
-        this.connect = function() {
+        this.connect = function () {
             console.warn("[DEBUG] VortexServerNode -- connect");
-          if(!node.connected && !node.connecting){
-              console.warn("[DEBUG] VortexServerNode -- connecting");
-              node.connecting = true;
-              console.log("[DEBUG] VortexServerNode -- connecting --> " + vortex.runtime);
-              node.client = new vortex.runtime.Runtime();
+            if (!node.connected && !node.connecting) {
+                console.warn("[DEBUG] VortexServerNode -- connecting");
+                node.connecting = true;
+                console.log("[DEBUG] VortexServerNode -- connecting --> " + vortex.runtime);
+                node.client = new vortex.runtime.Runtime();
 
-              node.client.onconnect = function() {
-                  console.warn("[DEBUG] VortexServerNode -- onconnect");
-                  node.connected = true;
-                  node.connecting = false;
-                  node.emit('connected');
-              };
+                node.client.onconnect = function () {
+                    console.warn("[DEBUG] VortexServerNode -- onconnect");
+                    node.connected = true;
+                    node.connecting = false;
+                    node.emit('connected');
+                };
 
-              node.client.connect(this.vortexurl, "uid:pwd");
-          }
+                node.client.connect(this.vortexurl, "uid:pwd");
+            }
         };
 
     }
+
     util.inherits(VortexServerNode, events.EventEmitter);
-    RED.nodes.registerType("prismtech-vortex", VortexServerNode,{
+    RED.nodes.registerType("prismtech-vortex", VortexServerNode, {
         credentials: {
-            user: {type:"text"},
-            password: {type:"password"}
+            user: {type: "text"},
+            password: {type: "password"}
         }
     });
 
     function ConfigureQos(qosContainer, node) {
-        if((typeof(node.partition) !== 'undefined') && (node.partition !== null)) {
+        if ((typeof(node.partition) !== 'undefined') && (node.partition !== null)) {
             qosContainer.add(node.vortexConn.vortex.Partition(node.partition));
         }
 
-        if((typeof(node.history) !== null) && (node.history !== null)) {
+        if ((typeof(node.history) !== null) && (node.history !== null)) {
             var kind = parseInt(node.history, 10);
-            if(kind == node.vortexConn.vortex.HistoryKind.KeepAll) {
+            if (kind == node.vortexConn.vortex.HistoryKind.KeepAll) {
                 qosContainer.add(node.vortexConn.vortex.History.KeepAll);
             } else {
                 qosContainer.add(node.vortexConn.vortex.History.KeepLast(parseInt(node.history_depth, 10)));
             }
         }
 
-        if((typeof(node.reliability) !== null) && (node.reliability !== null)) {
+        if ((typeof(node.reliability) !== null) && (node.reliability !== null)) {
             var kind = parseInt(node.reliability, 10);
-            if(kind == node.vortexConn.vortex.ReliabilityKind.BestEffort) {
+            if (kind == node.vortexConn.vortex.ReliabilityKind.BestEffort) {
                 qosContainer.add(node.vortexConn.vortex.Reliability.BestEffort);
             } else {
                 qosContainer.add(node.vortexConn.vortex.Reliability.Reliable);
             }
         }
 
-        if((typeof(node.content_filter) !== null) && (node.content_filter !== null)) {
+        if ((typeof(node.durability) !== null) && (node.durability !== null)) {
+            var kind = parseInt(node.durability, 10);
+            if (kind === node.vortexConn.vortex.DurabilityKind.Volatile) {
+                qosContainer.add(node.vortexConn.vortex.Durability.Volatile);
+            } else if (kind === node.vortexConn.vortex.DurabilityKind.TransientLocal) {
+                qosContainer.add(node.vortexConn.vortex.Durability.TransientLocal);
+            } else if (kind === node.vortexConn.vortex.DurabilityKind.Transient) {
+                qosContainer.add(node.vortexConn.vortex.Durability.Transient);
+            } else if (kind === node.vortexConn.vortex.DurabilityKind.Persistent) {
+                qosContainer.add(node.vortexConn.vortex.Durability.Persistent);
+            }
+        }
+
+
+        if ((typeof(node.content_filter) !== null) && (node.content_filter !== null)) {
             qosContainer.add(node.vortexConn.vortex.ContentFilter(node.content_filter));
         }
 
-        if((typeof(node.time_filter) !== null) && (node.time_filter !== null)) {
+        if ((typeof(node.time_filter) !== null) && (node.time_filter !== null)) {
             qosContainer.add(node.vortexConn.vortex.TimeFilter(node.time_filter));
         }
     }
 
     function VortexInNode(n) {
         console.warn("[VortexInNode]  -- creating");
-        RED.nodes.createNode(this,n);
+        RED.nodes.createNode(this, n);
+
+        this.status({fill: "red", shape: "ring", text: "Unconnected"});
+
         var topicSplit = n.topic.lastIndexOf('/');
         this.topic = n.topic.substring(topicSplit == 0 ? 0 : topicSplit + 1, n.topic.length);
-        this.partition = n.topic.substring(0,topicSplit) || null;
+        this.topicType = n.topicType;
+        this.partition = n.topic.substring(0, topicSplit) || null;
         this.domain = parseInt(n.domain) || 0;
         this.history = n.history || null;
         this.history_depth = n.history_depth || null;
         this.reliability = n.reliability || null;
+        this.durability = n.durability || null;
         this.content_filter = n.content_filter || null;
         this.time_filter = n.time_filter || null;
         this.vortexServer = n.vortex;
         this.vortexConn = RED.nodes.getNode(this.vortexServer);
+        var nodeReader = this;
+        var dr = null;
 
-        if(this.vortexConn) {
-            console.warn("[VortexInNode]  -- connecting -- " + JSON.stringify(this.vortexConn));
-            var node = this;
-            var createReader = function() {
-                var tqos = new node.vortexConn.vortex.TopicQos(node.vortexConn.vortex.Reliability.BestEffort);
-                console.warn("[VortexInNode]  -- topic -- " + node.topic);
-                var tdef = new node.vortexConn.vortex.Topic(node.domain, node.topic, tqos);
 
-                tdef.onregistered = function() {
-                    var drQos = new node.vortexConn.vortex.DataReaderQos();
+        if (nodeReader.vortexConn) {
+            console.warn("[VortexInNode]  -- connecting -- " + nodeReader.vortexConn.vortexurl);
 
-                    ConfigureQos(drQos, node);
+            var createReader = function () {
+                var tqos = new nodeReader.vortexConn.vortex.TopicQos(nodeReader.vortexConn.vortex.Reliability.BestEffort);
+                var tdef = new nodeReader.vortexConn.vortex.Topic(nodeReader.domain, nodeReader.topic, tqos, nodeReader.topicType);
 
-                    var dr = new node.vortexConn.vortex.DataReader(node.vortexConn.client, tdef, drQos);
+                tdef.onregistered = function () {
+                    var drQos = new nodeReader.vortexConn.vortex.DataReaderQos();
 
-                    dr.onconnect = function() {
-                        node.status({fill:"green", shape:"ring", text:"Connected"});
+                    ConfigureQos(drQos, nodeReader);
 
-                        dr.addListener(function(data){
-                            console.log(JSON.stringify(data));
-                            var msg = {};
-                            msg.payload = data;
-                            node.send(msg);
-                        });
+                    dr = new nodeReader.vortexConn.vortex.DataReader(node.vortexConn.client, tdef, drQos);
+
+                    dr.addListener(function (data) {
+                        var msg = {};
+                        msg.payload = data;
+                        msg.topic = nodeReader.topicType;
+                        nodeReader.send(msg);
+                    });
+
+                    dr.onconnect = function () {
+                        nodeReader.status({fill: "green", shape: "ring", text: "Connected"});
                     };
 
                 };
 
-                node.vortexConn.client.registerTopic(tdef);
+                nodeReader.vortexConn.client.registerTopic(tdef);
             };
 
-            if(this.vortexConn.connected == false) {
+            if (nodeReader.vortexConn.connected == false) {
+                nodeReader.status({fill: "yellow", shape: "ring", text: "Connecting"});
                 console.warn("[VortexInNode]  -- connecting");
 
 
-                node.vortexConn.on('connected', function() {
+                nodeReader.vortexConn.on('connected', function () {
                     console.warn("[VortexInNode]  -- createReader");
                     createReader();
                 });
 
-                node.vortexConn.register();
-                node.vortexConn.connect();
+                nodeReader.vortexConn.register();
+                nodeReader.vortexConn.connect();
 
                 console.warn("[VortexInNode]  -- connected");
             } else {
+                nodeReader.status({fill: "green", shape: "ring", text: "Connected"});
                 console.warn("[VortexInNode]  -- already connected");
                 console.warn("[VortexInNode]  -- createReader");
                 createReader();
             }
-
-
-
         }
+        else {
+        }
+        //});
+
+        nodeReader.on("close", function () {
+            console.warn("[VortexInNode] -- Close");
+        });
+
     }
+
     RED.nodes.registerType("prismtech-vortex-in", VortexInNode);
 
     function VortexOutNode(n) {
         console.warn("[VortexOutNode]  -- creating");
-        RED.nodes.createNode(this,n);
+        RED.nodes.createNode(this, n);
+
+        this.status({fill: "red", shape: "ring", text: "Unconnected"});
+
         var topicSplit = n.topic.lastIndexOf('/');
         this.topic = n.topic.substring(topicSplit == 0 ? 0 : topicSplit + 1, n.topic.length);
-        this.partition = n.topic.substring(0,topicSplit) || null;
+        this.topicType = n.topicType;
+        this.partition = n.topic.substring(0, topicSplit) || null;
         this.domain = parseInt(n.domain) || 0;
         this.history = n.history || null;
         this.history_depth = n.history_depth || null;
         this.reliability = n.reliability || null;
+        this.durability = n.durability || null;
         this.vortexServer = n.vortex;
         this.vortexConn = RED.nodes.getNode(this.vortexServer);
+        var dw = null;
+        var nodeWriter = this;
 
-        if(this.vortexConn) {
-            console.warn("[VortexOutNode]  -- connecting -- " + JSON.stringify(this.vortexConn));
-            var node = this;
-            var createWriter = function() {
-                var tqos = new node.vortexConn.vortex.TopicQos(node.vortexConn.vortex.Reliability.BestEffort);
-                console.warn("[VortexOutNode]  -- topic -- " + node.topic);
-                var tdef = new node.vortexConn.vortex.Topic(node.domain, node.topic, tqos);
+        nodeWriter.on('input', function (msg) {
 
-                tdef.onregistered = function() {
-                    var dwQos = new node.vortexConn.vortex.DataWriterQos();
+            //if (msg.topic == 'connect' && msg.payload == 'on') {
+            if (nodeWriter.vortexConn) {
+                console.warn("[VortexOutNode]  -- connecting -- " + nodeWriter.vortexConn.vortexurl);
 
-                    ConfigureQos(dwQos, node);
+                var createWriter = function () {
+                    var tqos = new nodeWriter.vortexConn.vortex.TopicQos(nodeWriter.vortexConn.vortex.Reliability.BestEffort);
+                    console.warn("[VortexOutNode]  -- topic -- " + nodeWriter.topic + " [" + nodeWriter.topicType + "]");
+                    var tdef = new nodeWriter.vortexConn.vortex.Topic(nodeWriter.domain, nodeWriter.topic, tqos, nodeWriter.topicType);
 
-                    var dw = new node.vortexConn.vortex.DataWriter(node.vortexConn.client, tdef, dwQos);
+                    tdef.onregistered = function () {
+                        var dwQos = new nodeWriter.vortexConn.vortex.DataWriterQos();
 
-                    dw.onconnect = function() {
-                        node.status({fill:"green", shape:"ring", text:"Connected"});
+                        ConfigureQos(dwQos, nodeWriter);
+
+                        var dw = new nodeWriter.vortexConn.vortex.DataWriter(nodeWriter.vortexConn.client, tdef, dwQos);
+
+                        dw.onconnect = function () {
+                            nodeWriter.status({fill: "green", shape: "ring", text: "Connected"});
+                        };
+
+                        nodeWriter.on("input", function (msg) {
+                            dw.write(msg.payload);
+                        });
                     };
 
-                    node.on("input", function(msg){
-                        dw.write(msg.payload);
-                    });
+                    nodeWriter.vortexConn.client.registerTopic(tdef);
                 };
 
-                node.vortexConn.client.registerTopic(tdef);
-            };
+                if (nodeWriter.vortexConn.connected == false) {
+                    console.warn("[VortexOutNode]  -- connecting");
+                    nodeWriter.vortexConn.on('connected', function () {
+                        console.warn("[VortexOutNode]  -- createWriter");
+                        createWriter();
+                    });
 
-            if(this.vortexConn.connected == false) {
-                console.warn("[VortexOutNode]  -- connecting");
-                node.vortexConn.on('connected', function() {
-                    console.warn("[VortexInNode]  -- createWriter");
+                    nodeWriter.vortexConn.register();
+                    nodeWriter.vortexConn.connect();
+                    console.warn("[VortexOutNode]  -- connected");
+                } else {
+                    console.warn("[VortexOutNode]  -- already connected");
+                    console.warn("[VortexOutNode]  -- createWriter");
                     createWriter();
-                });
-
-                node.vortexConn.register();
-                node.vortexConn.connect();
-                console.warn("[VortexOutNode]  -- connected");
-            } else {
-                console.warn("[VortexOutNode]  -- already connected");
-                console.warn("[VortexOutNode]  -- createWriter");
-                createWriter();
+                }
             }
-        }
+        });
+
+        nodeWriter.on("close", function () {
+            console.warn("[VortexOutNode] -- Close");
+        });
+
     }
+
     RED.nodes.registerType("prismtech-vortex-out", VortexOutNode);
 }
